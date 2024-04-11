bbf29e14738118aa18a622130032302af7d66481
Josué López <jomalolep@gmail.com>
2024-04-11 15:09:15 -0600

    fix: exclude typescript

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app'
 
-const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
